43590c4c63a8d9f607e27a57a4b4fed732a7668a
sf2987 <54997007+sf2987@users.noreply.github.com>
2019-10-30 09:17:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Our labels along the x-axis
-var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+var years = [2014,2015,2016,2017,2018];
 // For drawing the lines
-var africa = [86,114,106,106,107,111,133,221,783,2478];
-var asia = [282,350,411,502,635,809,947,1402,3700,5267];
-var europe = [168,170,178,190,203,276,408,547,675,734];
-var latinAmerica = [40,20,10,16,24,38,74,167,508,784];
-var northAmerica = [6,3,2,2,7,26,82,172,312,433];
+var timesSq = [86,114,106,106,107,111,133,221,783,2478];
+var grandCentral = [282,350,411,502,635,809,947,1402,3700,5267];
+var heraldSquare = [168,170,178,190,203,276,408,547,675,734];
+var unionSquare = [40,20,10,16,24,38,74,167,508,784];
+var pennStation = [6,3,2,2,7,26,82,172,312,433];
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
@@ -14,32 +14,32 @@ var myChart = new Chart(ctx, {
     labels: years,
     datasets: [
       {
-        data: africa,
-        label: "Africa",
+        data: timesSq,
+        label: "Times Sq-42 St",
         borderColor: "#3e95cd",
         fill: false
       },
       {
-        data: asia,
-        label: "Asia",
+        data: grandCentral,
+        label: "Grand Central-42 St",
         borderColor: "#8e5ea2",
         fill: false
       },
       {
-        data: europe,
-        label: "Europe",
+        data: heraldSquare,
+        label: "34 St-Herald Square",
         borderColor: "#3cba9f",
         fill: false
       },
       {
-        data: latinAmerica,
-        label: "Latin America",
+        data: unionSquare,
+        label: "14 St-Union Square",
         borderColor: "#e8c3b9",
         fill: false
       },
       {
-        data: northAmerica,
-        label: "North America",
+        data: pennStation,
+        label: "34 St-Penn Station",
         borderColor: "#c45850",
         fill: false
       }
