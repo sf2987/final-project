919dc36c1f0061856d518670b33a398740efcef5
sf2987 <54997007+sf2987@users.noreply.github.com>
2019-11-05 09:20:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -45,4 +45,11 @@ var myChart = new Chart(ctx, {
       }
     ]
   }
+	function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
 });
